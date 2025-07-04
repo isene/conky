57e42fcd3d5428e2eb1b89679bdac0fa0e84bd6c
Geir <g@isene.com>
2025-07-04 11:21:06 +0200

    Compacted conky line, updated documentation

--- scripts/essid.sh
+++ scripts/essid.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Get ESSID of active interface
 essid=$(iw dev | awk '/ssid/ {print $2}')
 essidlength=${#essid}
 
-if [ $essidlength -lt 13 ]
-then
-	echo $essid
+# Shorten ESSID if needed
+if [ $essidlength -lt 9 ]; then
+    display_essid=$essid
 else
-	echo $essid | cut -c 1-12 | sed "s/$/../"
+    display_essid=$(echo $essid | cut -c 1-8)..
 fi
+
+# Get external IP (with timeout and silent flags)
+ip=$(curl -4 -s --max-time 2 ifconfig.me)
+
+# Output for Conky (single line or split with \n if preferred)
+echo "$display_essid $ip"
+
